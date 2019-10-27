46d94470409f7fa34a3b4261af62bcdd21a64744
Deneb-Algedi <46696752+Deneb-Algedi@users.noreply.github.com>
2019-10-27 17:44:23 -0400

    Create stackedBars.php

---
+++ stackedBars.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html>
+<?php 
+      session_start();
+
+      if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] == false) {
+        header("Location: index.php");
+      }
+  
+      ?>
+<head>
+<style>
+  .axis .domain {
+    display: none;
+  }
+  
+</style>
+
+<link href="nav.css" rel="stylesheet" type="text/css">
+
+</head>
+
+
+<!-- The navigation menu -->
+
+ <ul>
+  <li><a href="home.php">Home</a></li>
+  <li><a href="port-form.php">Ports Stacked Bars</a></li>
+  <li><a href="cal-form.php">Ports Calendar View</a></li>
+  <li style="float:right"><a class="active" href="logout.php">Log Out</a></li>
+</ul> 
+
+<h2 id="h">Ports Stacked Bar Visual</h2>
+
+<svg width="1200" height="750"></svg>
+<script src="https://d3js.org/d3.v5.min.js"></script>
+
+
+
+<body>
+
+<script>
+
+
+//set up svg
+  var svg = d3.select("svg"),
+    margin = {
+      top: 20,
+      right: 20,
+      bottom: 30,
+      left: 40
+    },
+
+    width = +svg.attr("width") - margin.left - margin.right - 40,
+
+    height = +svg.attr("height") - margin.top - margin.bottom,
+
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var x = d3.scaleBand()
+    .rangeRound([0, width])
+    .paddingInner(0.05)
+    .align(0.1);
+
+  var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
+
+  var z = d3.scaleOrdinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00", "#e36719", "#e34119", "#e31919"]);
+
+d3.json("grouped-ports.json").then(function(data) {
+
+ 
+  var date1 = new Date("2017/01/01");
+
+  var url_string= window.location.href;
+  var url = new URL(url_string);
+  var date2str = url.searchParams.get("sdate");
+  var date2 = new Date(date2str);
+
+  var difference = Math.abs(date2 - date1);
+ 
+  var index = Math.ceil(difference / (1000 * 60 * 60 * 24)); 
+  
+
+  date = data[index][date2str];
+
+   const dataArray = Object.keys(date).map(i => date[i]);
+
+  
+  //recognize keys
+  var keys = ["100", "200", "300", "400", "500", "600", "700", "800", "900", "1023"];
+
+  var hours = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"];
+
+  
+  //for each hour sum total
+  dataArray.forEach(function(d){
+    d.total = 0;
+    keys.forEach(function(k){
+      d.total += d[k];
+    })
+  });
+
+  //
+  dataArray.sort(function(a, b) {
+    return b.total - a.total;
+  });
+
+  x.domain(hours.map(function(d) {
+    return d;
+  }));
+
+  y.domain([0, d3.max(dataArray, function(d) {
+    return d.total;
+  })]).nice();
+  z.domain(keys);
+
+  g.append("g")
+    .selectAll("g")
+    .data(d3.stack().keys(keys)(dataArray))
+    .enter().append("g")
+    .attr("fill", function(d) {
+      return z(d.key);
+      
+    })
+    .selectAll("rect")
+    .data(function(d) {
+      return d;
+    })
+    .enter().append("rect")
+    .attr("x", function(d) {   
+     return x(d.data.hour);
+      
+   })
+    .attr("y", function(d) {
+      return y(d[1]);
+    })
+    .attr("height", function(d) {
+      return y(d[0]) - y(d[1]);
+    })
+    .attr("width", x.bandwidth())   
+
+    .on("mouseover", function() { tooltip.style("display", null); })
+    .on("mouseout", function() { tooltip.style("display", "none"); })
+    .on("mousemove", function(d) {
+      console.log(d);
+      var xPosition = d3.mouse(this)[0] - 5;
+      var yPosition = d3.mouse(this)[1] - 5;
+      tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+      tooltip.select("text").text(d[1]-d[0]);
+    }) ;
+
+  g.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  g.append("g")
+    .attr("class", "axis")
+    .call(d3.axisLeft(y).ticks(null, "s"))
+    .append("text")
+    .attr("x", 2)
+    .attr("y", y(y.ticks().pop()) + 0.5)
+    .attr("dy", "0.32em")
+    .attr("fill", "#000")
+    .attr("font-weight", "bold")
+    .attr("text-anchor", "start")
+    .text("Port connetions");
+
+
+//legend
+  var legend = g.append("g")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", 12)
+    .attr("text-anchor", "end")
+    .selectAll("g")
+    .data(keys.slice().reverse())
+    .enter().append("g")
+    .attr("transform", function(d, i) {
+      return "translate(0," + i * 20 + ")";
+    });
+
+  legend.append("rect")
+    .attr("x", width + 25)
+    .attr("width", 19)
+    .attr("height", 19)
+    .attr("fill", z);
+
+  legend.append("text")
+    .attr("x", width + 20)
+    .attr("y", 9.5)
+    .attr("dy", "0.32em")
+    .text(function(d) {
+      return d;
+  });
+});
+
+// Prep the tooltip bits, initial display is hidden
+var tooltip = svg.append("g")
+  .attr("class", "tooltip")
+  .style("display", "none");
+    
+tooltip.append("rect")
+  .attr("width", 50)
+  .attr("height", 25)
+  .attr("fill", "white")
+  .style("opacity", 0.5);
+
+tooltip.append("text")
+  .attr("x", 25)
+  .attr("dy", "1.2em")
+  .style("text-anchor", "middle")
+  .attr("font-size", "12px")
+  .attr("font-weight", "bold");
+
+</script>
+</body>
+</html>
